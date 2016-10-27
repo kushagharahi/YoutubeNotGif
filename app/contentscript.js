79ce1cef2e8ded27d23c2034e2b039efe37c978e
Kusha Gharahi <kusha95@gmail.com>
2016-10-27 18:01:28 -0500

    Committing to continue work on laptop.

--- app/contentscript.js
+++ app/contentscript.js
@@ -1,10 +1,18 @@
     var a = document.getElementsByTagName('a');
     var youtubeTag = "<span style='color: #FF1493;font-weight: bold;'>[YOUTUBE] </span>";
     for (var i = 0; i < a.length; i++) {
-        var a_attr = a[i].getAttribute('data-href-url');
-        if (a_attr !== null)
+        var a_attr = a[i].getAttribute('class');
+        if (a_attr.includes("title"))
             if (a_attr.indexOf("youtube.com") > -1 || a_attr.indexOf("youtu.be") > -1) {
                 a[i].innerHTML = youtubeTag + a[i].innerHTML;
+                break;
             }
+        
     }
-  
+    // var getYoutubeViews = function() {
+
+    // }
+
+    // var getAttibutes = function(){
+        
+    // }

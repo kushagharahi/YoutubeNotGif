4a2fc1fd71145b9665f099138ffffed26dcfc660
Kusha Gharahi <kusha95@gmail.com>
2016-10-27 21:24:23 -0500

    added a basic settings page where you can choose the color to set

--- app/contentscript.js
+++ app/contentscript.js
@@ -1,19 +1,14 @@
-    var a = document.getElementsByTagName('a');
-    var youtubeTag = "<span style='color: #FF1493;font-weight: bold;'>[YOUTUBE] </span>";
-    for (var i = 0; i < a.length; i++) {
-        var a_attr = a[i].getAttribute('class');
-        if (a_attr !== null && a_attr.includes("title")) {
-		var href = a[i].getAttribute('href');
-		if (href.indexOf("youtube.com") > -1 || href.indexOf("youtu.be") > -1) {
-                	a[i].innerHTML = youtubeTag + a[i].innerHTML;
-            }
+chrome.storage.sync.get({'color': '#FF1493'}, function (item) {
+	var a = document.getElementsByTagName('a');
+	var youtubeTag = "<span style='color:" + item.color + ";font-weight: bold;'>[YOUTUBE] </span>";
+	for (var i = 0; i < a.length; i++) {
+		var a_attr = a[i].getAttribute('class');
+		if (a_attr !== null && a_attr.includes('title')) {
+			var href = a[i].getAttribute('href');
+			if (href.indexOf('youtube.com') > -1 || href.indexOf('youtu.be') > -1) {
+				a[i].innerHTML = youtubeTag + a[i].innerHTML;
+			}
+		}
+
 	}
-        
-    }
-    // var getYoutubeViews = function() {
-
-    // }
-
-    // var getAttibutes = function(){
-        
-    // }
+});

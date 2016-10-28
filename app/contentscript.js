442dcf35ba3bff08b2050b34bb4c5955c6fb7575
Kusha Gharahi <kusha95@gmail.com>
2016-10-27 19:14:04 -0500

    reddit update broke extention, now fixed

--- app/contentscript.js
+++ app/contentscript.js
@@ -2,11 +2,12 @@
     var youtubeTag = "<span style='color: #FF1493;font-weight: bold;'>[YOUTUBE] </span>";
     for (var i = 0; i < a.length; i++) {
         var a_attr = a[i].getAttribute('class');
-        if (a_attr.includes("title"))
-            if (a_attr.indexOf("youtube.com") > -1 || a_attr.indexOf("youtu.be") > -1) {
-                a[i].innerHTML = youtubeTag + a[i].innerHTML;
-                break;
+        if (a_attr !== null && a_attr.includes("title")) {
+		var href = a[i].getAttribute('href');
+		if (href.indexOf("youtube.com") > -1 || href.indexOf("youtu.be") > -1) {
+                	a[i].innerHTML = youtubeTag + a[i].innerHTML;
             }
+	}
         
     }
     // var getYoutubeViews = function() {
